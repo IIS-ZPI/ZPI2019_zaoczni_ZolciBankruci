d1fb1d16e59c89483db3e0e2bc6d3707ba5022bf
187574 <187574@CARBON.CTI.P.LODZ.PL>
2019-12-15 13:42:16 +0100

    dzielenie

--- ZolciBankruci/src/com/Bankruci/Calc.java
+++ ZolciBankruci/src/com/Bankruci/Calc.java
@@ -1,4 +1,9 @@
 package com.Bankruci;
 
-public class Calc {
+public class Calc implements IArithmeticsDiv {
+
+    @Override
+    public double Division(double A, double B) {
+        return A/B;
+    }
 }
