21699f201608f2fc2da094b40baaef58211c65b3
Zbyszek212322 <zbyszrad@poczta.wp.pl>
2019-12-15 13:43:46 +0100

    zad 4

--- ZolciBankruci/src/com/Bankruci/Calc.java
+++ ZolciBankruci/src/com/Bankruci/Calc.java
@@ -1,4 +1,10 @@
 package com.Bankruci;
 
-public class Calc {
+public class Calc implements IArithmeticsMult{
+
+
+    @Override
+    public double Multiplication(double A, double B) {
+        return A * B;
+    }
 }
