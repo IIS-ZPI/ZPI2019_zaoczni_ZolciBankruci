f2ab042be5762d744ef81fd5a8b2fbce2c056eb7
Jakub Kryska <kryska.jakub.optilean@outlook.com>
2020-01-12 01:53:24 +0100

    branch a comm

--- ZolciBankruci/src/com/Bankruci/IArithmeticsAdd.java
+++ ZolciBankruci/src/com/Bankruci/IArithmeticsAdd.java
@@ -1,5 +1,6 @@
 package com.Bankruci;
 
 public interface IArithmeticsAdd {
-    double Addition(double A, double B);
+    double Addition(double A, double B);    // addition interface
+    // A comment
 }
