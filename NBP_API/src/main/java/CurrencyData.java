ad74b9f25d0314153a74cb734184d7c15b0d09d2
solid <solidsrv@gmail.com>
2020-01-14 03:52:29 +0100

    getPairRates functionality and tests, enum for last quater

--- NBP_API/src/main/java/CurrencyData.java
+++ NBP_API/src/main/java/CurrencyData.java
@@ -7,50 +7,90 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class CurrencyData {
+    private Currencies currency;
+    private Currencies currencyPair;
+    private boolean currencyPairs = false;
 
-    Currencies currency;
-
-    CurrencyData(String curr, int days) {
-
-        try {
-            Client client = Client.create();
-            WebResource webResource = client.resource("http://api.nbp.pl/api/exchangerates/rates/a/" + curr + "/last/" + days + "/");
-            ClientResponse webResponse = webResource.accept("application/json").get(ClientResponse.class);
-            if (webResponse.getStatus() != 200) {
-                throw new RuntimeException("Błąd HTTP ..." + webResponse.getStatus());
-            }
-            String receivedJson = webResponse.getEntity(String.class);
-            ObjectMapper mapper = new ObjectMapper();
-            currency = mapper.readValue(receivedJson, Currencies.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    CurrencyData(String curr, int sessions) {
+        WebResource webResource = Client.create().resource("http://api.nbp.pl/api/exchangerates/rates/a/" + curr + "/last/" + sessions + "/");
+        this.setCurrency(webResource);
     }
 
     CurrencyData(String curr, LocalDate date1, LocalDate date2) {
+        WebResource webResource = Client.create().resource("http://api.nbp.pl/api/exchangerates/rates/a/" + curr + "/"
+                + date1.toString() + "/" + date2.toString() + "/");
+        this.setCurrency(webResource);
+    }
+
+    CurrencyData(String currency1, String currency2, LocalDate date1, LocalDate date2) {
+        currencyPairs = true;
+        WebResource webResource1 = Client.create().resource("http://api.nbp.pl/api/exchangerates/rates/a/" + currency1 + "/"
+                + date1.toString() + "/" + date2.toString() + "/");
+        WebResource webResource2 = Client.create().resource("http://api.nbp.pl/api/exchangerates/rates/a/" + currency2 + "/"
+                + date1.toString() + "/" + date2.toString() + "/");
+        this.setCurrency(webResource1, webResource2);
+    }
+
+    private void setCurrency(WebResource webResource){
         try {
-            Client client = Client.create();
-            WebResource webResource = client.resource("http://api.nbp.pl/api/exchangerates/rates/a/" + curr + "/"
-                    + date1.toString() + "/" + date2.toString() + "/");
             ClientResponse webResponse = webResource.accept("application/json").get(ClientResponse.class);
             if (webResponse.getStatus() != 200) {
                 throw new RuntimeException("Błąd HTTP ..." + webResponse.getStatus());
             }
             String receivedJson = webResponse.getEntity(String.class);
             ObjectMapper mapper = new ObjectMapper();
+
             currency = mapper.readValue(receivedJson, Currencies.class);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public ArrayList getRates() {
-        ArrayList rates = new ArrayList();
+    private void setCurrency(WebResource webResource1, WebResource webResource2){
+        try {
+            ClientResponse webResponse1 = webResource1.accept("application/json").get(ClientResponse.class);
+            ClientResponse webResponse2 = webResource2.accept("application/json").get(ClientResponse.class);
+            if (webResponse1.getStatus() != 200 || webResponse2.getStatus() != 200) {
+                throw new RuntimeException("Błąd HTTP ...\nFirst currency status: " + webResponse1.getStatus() + "\nSecond currency status: " + webResponse2.getStatus());
+            }
+            String receivedJson1 = webResponse1.getEntity(String.class);
+            String receivedJson2 = webResponse2.getEntity(String.class);
+            ObjectMapper mapper = new ObjectMapper();
 
-        for (int i = 0; i < currency.getRates().size(); i++) {
-            rates.add(currency.getRates().get(i).getMid());
+            currency = mapper.readValue(receivedJson1, Currencies.class);
+            currencyPair = mapper.readValue(receivedJson2, Currencies.class);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return rates;
+    }
+
+    public ArrayList<Double> getRates() {
+        if(currencyPairs){
+            return null;
+        } else {
+            ArrayList<Double> rates = new ArrayList<>();
+
+            for (int i = 0; i < currency.getRates().size(); i++) {
+                rates.add(currency.getRates().get(i).getMid());
+            }
+            return rates;
+        }
+    }
+    public ArrayList<ArrayList<Double>> getPairRates() {
+        if(currencyPairs){
+            ArrayList<ArrayList<Double>> rates = new ArrayList<>();
+            if(currency.getRates().size() == currency.getRates().size()) {
+                for (int i = 0; i < currency.getRates().size(); i++) {
+                    ArrayList<Double> sameDayRate = new ArrayList<>();
+                    sameDayRate.add(currency.getRates().get(i).getMid());
+                    sameDayRate.add(currencyPair.getRates().get(i).getMid());
+                    rates.add(sameDayRate);
+                }
+            }
+            return rates;
+        } else {
+            return null;
+        }
     }
 
 }
--- NBP_API/src/test/java/CurrencyDataTest.java
+++ NBP_API/src/test/java/CurrencyDataTest.java
@@ -2,8 +2,10 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CurrencyDataTest {
 
@@ -20,6 +22,17 @@ public class CurrencyDataTest {
         LocalDate date2 = LocalDate.parse("2020-01-03", formatter);
 
         CurrencyData result2 = new CurrencyData(currencySymbol.toString(), date1, date2);
-        assertEquals(3.8213, result2.getRates().get(0));
+        assertEquals(3.8213, result2.getRates().get(0), 0);
+    }
+
+    @Test
+    public void getPairRates() {
+        CurrencyData result = new CurrencyData(CurrencySymbols.EUR.toString(), CurrencySymbols.USD.toString(), LocalDate.of(2020, 1, 12), LocalDate.of(2020, 1, 13));
+        ArrayList<ArrayList<Double>> arrayLists = new ArrayList<>();
+        ArrayList<Double> rates = new ArrayList<>();
+        rates.add(4.2353);
+        rates.add(3.8101);
+        arrayLists.add(rates);
+        assertEquals(arrayLists, result.getPairRates());
     }
 }
