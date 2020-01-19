955a9701ebdfff0e1895feadfdfd24568a37cdaa
Wiktor <wiktor.nowacki95@gmail.com>
2020-01-19 09:58:18 +0100

    fix date in tests

--- NBP_API/src/test/java/StatisticalMeasuresTest.java
+++ NBP_API/src/test/java/StatisticalMeasuresTest.java
@@ -8,7 +8,7 @@ public class StatisticalMeasuresTest {
 
     @Test
     public void measuresTest() {
-        CurrencyData currencyData = new CurrencyData(CurrencySymbols.EUR.toString(), LocalDate.of(2020, 1, 7), LocalDate.of(2020, 1, 14));
+        CurrencyData currencyData = new CurrencyData(CurrencySymbols.EUR.toString(), LocalDate.of(2020, 1, 7), LocalDate.of(2020, 1, 13));
         StatisticalMeasures statisticalMeasures = new StatisticalMeasures(currencyData);
         assertEquals(4.2459, statisticalMeasures.getMedian(), 0);
         assertEquals(4.2347, statisticalMeasures.getDominant(), 0);
