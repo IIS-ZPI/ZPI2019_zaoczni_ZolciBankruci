ec43b2979fbeb67d0e4d7879c7d4d0d6ae3b58fe
Jakub Wiśniewski <jakubwisn@gmail.com>
2020-01-19 03:55:56 +0100

    Adding javafx to project

--- NBP_API/src/main/java/Main.java
+++ NBP_API/src/main/java/Main.java
@@ -1,20 +1,34 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("NBP Zolci Bankruci");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
 
-        CurrencySymbols currencySymbol = (CurrencySymbols.USD);
+        launch(args);
 
-        CurrencyData result1 = new CurrencyData(currencySymbol.toString(), 3);
-        System.out.println(result1.getRates());
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate date1 = LocalDate.parse("2020-01-03", formatter);
-        LocalDate date2 = LocalDate.parse("2020-01-03", formatter);
-
-        CurrencyData result2 = new CurrencyData(currencySymbol.toString(), date1, date2);
-        System.out.println(result2.getRates());
+//        CurrencySymbols currencySymbol = (CurrencySymbols.USD);
+//
+//        CurrencyData result1 = new CurrencyData(currencySymbol.toString(), 3);
+//        System.out.println(result1.getRates());
+//
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        LocalDate date1 = LocalDate.parse("2020-01-03", formatter);
+//        LocalDate date2 = LocalDate.parse("2020-01-03", formatter);
+//
+//        CurrencyData result2 = new CurrencyData(currencySymbol.toString(), date1, date2);
+//        System.out.println(result2.getRates());
     }
 }
