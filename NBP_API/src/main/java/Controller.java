9b022d5a6c16fcd09635c4f1a7b2991af7d08a4d
Jakub Wiśniewski <jakubwisn@gmail.com>
2020-01-29 00:48:03 +0100

    Rebuild javaFx

--- NBP_API/src/main/java/Controller.java
+++ NBP_API/src/main/java/Controller.java
@@ -1,11 +1,14 @@
-
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 
 public class Controller {
@@ -17,7 +20,10 @@ public class Controller {
     Label label;
 
     @FXML
-    LineChart chartMonth;
+    LineChart chartPeriod;
+
+    @FXML
+    NumberAxis numberAxis, numberAxis2;
 
 
     public void iloscSesji() {
@@ -54,7 +60,31 @@ public class Controller {
         CurrencyData currencyDataLastQuater = new CurrencyData(CurrencySymbols.EUR.toString(), CurrencySymbols.USD.toString(), dateLastQuater, dateLastQuater.plusMonths(3));
         ChangesDistributor changesDistributorLastMonth = new ChangesDistributor(currencyDataLastMonth);
         ChangesDistributor changesDistributorLastQuater = new ChangesDistributor(currencyDataLastQuater);
-        label.setText(changesDistributorLastMonth.getChanges().toString());
-        label.setText(changesDistributorLastQuater.getChanges().toString());
+        XYChart.Series seriesQuarter = new XYChart.Series();
+        XYChart.Series seriesMonth = new XYChart.Series();
+        seriesQuarter.setName("Last Quarter");
+        seriesMonth.setName("Last Month");
+        numberAxis.setAutoRanging(true);
+        List<Double> lastQuarterChanges = changesDistributorLastQuater.getChanges();
+        List<Double> lastMonthChanges = changesDistributorLastMonth.getChanges();
+        System.out.println(lastQuarterChanges.size());
+        Map<Integer, Double> changesQuarterMap = new TreeMap<>();
+        for (int i = 1; i < lastQuarterChanges.size(); i++) {
+            changesQuarterMap.put(i, lastQuarterChanges.get(i));
+        }
+        for (Map.Entry<Integer, Double> entry : changesQuarterMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+            seriesQuarter.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+        }
+        Map<Integer, Double> changesMonthMap = new TreeMap<>();
+        for (int i = 1; i < lastMonthChanges.size(); i++) {
+            changesMonthMap.put(i, lastQuarterChanges.get(i));
+        }
+        for (Map.Entry<Integer, Double> entry : changesMonthMap.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+            seriesMonth.getData().add(new XYChart.Data(entry.getKey(), entry.getValue()));
+        }
+        chartPeriod.getData().addAll(seriesQuarter, seriesMonth);
+
     }
 }
--- NBP_API/src/main/java/Main.java
+++ NBP_API/src/main/java/Main.java
@@ -8,15 +8,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample2.fxml"));
         primaryStage.setTitle("NBP API");
         primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
-
-
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
