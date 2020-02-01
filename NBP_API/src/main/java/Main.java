51ff8783e8ae37eae41e540ad9ec30a1e0781ce6
solid <solidsrv@gmail.com>
2020-02-01 19:21:24 +0100

    javafx jdk8 pipeline test 3

--- NBP_API/src/main/java/Main.java
+++ NBP_API/src/main/java/Main.java
@@ -2,13 +2,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample2.fxml"));
         primaryStage.setTitle("NBP API");
         primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
